d68dc36f47e6fef8646d73f4dde36c4c30fd52ad
marina <marinakovaleva0@gmail.com>
2024-12-02 14:46:20 +0900

    correct README.md

--- gocadgo/network.py
+++ gocadgo/network.py
@@ -12,29 +12,29 @@ class Network:
         length: by number of cells
         config :
         """
-
         # set boundary conditions to default if user hasn't specified them:
         if boundary is None:
             boundary = set_boundary()
-        print(boundary)
+
+        self.create_network(height, width, length, boundary)
+        self.run_network()
+
+
+    def create_network(self, height, width, length, boundary):
         # the first row of cells should always be start cells:
         start_grid_size = (1, width, length)
-        init_startCell = StartCell(**boundary) # boundary condition stored as dict
+        init_startCell = StartCell(**boundary)  # boundary condition stored as dict
         start_network = np.full(start_grid_size, init_startCell, dtype=object)
 
         # now normal cells:
-        grid_size = (height-1, width, length)
-        init_Cell = StartCell(**boundary) # boundary condition stored as dict
+        grid_size = (height - 1, width, length)
+        init_Cell = StartCell(**boundary)  # boundary condition stored as dict
         network = np.full(grid_size, init_Cell, dtype=object)
 
         # Combine the two networks (start_network + network)
         self.network = np.concatenate((start_network, network), axis=0)
-        print(self.network.shape)
 
-        print(self.network)
-
-        # use Numpy ops over vectorisation:
-
+    def run_network(self):
         # Access a cell:
         print(f"Cell at (0, 0, 0): {self.network[0, 0, 0].T_in}")
         print(f"Cell at (10, 4, 5): {self.network[6, 4, 5].T_in}")
@@ -44,4 +44,3 @@ class Network:
         self.network[0, 0, 0]._cp = 2.0
 
         print(f"Modified Cell at (0, 0, 0): {self.network[0, 0, 0]}")
-
